0b1015ab41c07f5011ffd93218625045ad8be0c2
sles10604-cpu <sles10604@gmail.com>
2026-07-30 22:12:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
-// CORS ayarı: Netlify'dan gelen Socket bağlantılarına izin verir
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -16,33 +15,41 @@ const io = new Server(server, {
 let players = {};
 
 io.on("connection", (socket) => {
-  console.log("Yeni oyuncu bağlandı:", socket.id);
+  console.log("Oyuncu bağlandı:", socket.id);
 
-  // Yeni oyuncuyu ekle
-  players[socket.id] = {
-    x: Math.floor(Math.random() * 400) + 50,
-    y: Math.floor(Math.random() * 250) + 50,
-    color: '#' + Math.floor(Math.random()*16777215).toString(16)
-  };
+  // Oyuncu adını kaydetme ve oyuna dahil etme
+  socket.on("joinGame", (playerName) => {
+    players[socket.id] = {
+      id: socket.id,
+      name: playerName || "Sürücü",
+      x: (Math.random() - 0.5) * 8, // Rastgele şerit
+      z: -5,
+      color: '#' + Math.floor(Math.random() * 16777215).toString(16)
+    };
 
-  // Tüm bağlı istemcilere güncel oyuncuları gönder
-  io.emit("stateUpdate", players);
+    // Bağlanan oyuncuya kendi Socket ID'sini gönder
+    socket.emit("init", socket.id);
 
-  // İstemciden gelen hareket verisi
-  socket.on("move", (data) => {
+    // Herkese güncel oyuncu listesini yayınla
+    io.emit("stateUpdate", players);
+  });
+
+  // Oyuncudan gelen anlık konum güncellemesi (x, z pozisyonu)
+  socket.on("playerMove", (data) => {
     if (players[socket.id]) {
-      players[socket.id].x += data.x;
-      players[socket.id].y += data.y;
-      io.emit("stateUpdate", players);
+      players[socket.id].x = data.x;
+      players[socket.id].z = data.z;
+      // Performans için canlı veriyi diğer oyunculara dağıtıyoruz
+      socket.broadcast.emit("playerMoved", players[socket.id]);
     }
   });
 
-  // Oyuncu ayrılınca sil
+  // Oyuncu ayrıldığında
   socket.on("disconnect", () => {
     delete players[socket.id];
-    io.emit("stateUpdate", players);
+    io.emit("playerLeft", socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Sunucu ${PORT} portunda çalışıyor.`));
+server.listen(PORT, () => console.log(`Sunucu ${PORT} portunda aktif.`));

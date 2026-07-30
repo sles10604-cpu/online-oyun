6b445d7aa501fc254b64cc26d4cb208d8d217019
sles10604-cpu <sles10604@gmail.com>
2026-07-30 22:18:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,39 +17,38 @@ let players = {};
 io.on("connection", (socket) => {
   console.log("Oyuncu bağlandı:", socket.id);
 
-  // Oyuncu adını kaydetme ve oyuna dahil etme
+  // Oyuncu katıldığında
   socket.on("joinGame", (playerName) => {
     players[socket.id] = {
       id: socket.id,
       name: playerName || "Sürücü",
-      x: (Math.random() - 0.5) * 8, // Rastgele şerit
-      z: -5,
+      x: (Math.random() - 0.5) * 6, // Şeritte sabit/yok yakın x
+      z: -5, // Herkes aynı başlangıç Z noktasında başlar
       color: '#' + Math.floor(Math.random() * 16777215).toString(16)
     };
 
-    // Bağlanan oyuncuya kendi Socket ID'sini gönder
     socket.emit("init", socket.id);
-
-    // Herkese güncel oyuncu listesini yayınla
     io.emit("stateUpdate", players);
   });
 
-  // Oyuncudan gelen anlık konum güncellemesi (x, z pozisyonu)
+  // Konum güncellemesi
   socket.on("playerMove", (data) => {
     if (players[socket.id]) {
       players[socket.id].x = data.x;
       players[socket.id].z = data.z;
-      // Performans için canlı veriyi diğer oyunculara dağıtıyoruz
-      socket.broadcast.emit("playerMoved", players[socket.id]);
     }
   });
 
-  // Oyuncu ayrıldığında
   socket.on("disconnect", () => {
     delete players[socket.id];
     io.emit("playerLeft", socket.id);
   });
 });
+
+// Tüm cihazlar birbirini görsün diye saniyede 20 kez durum senkronizasyonu
+setInterval(() => {
+  io.emit("stateUpdate", players);
+}, 50);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Sunucu ${PORT} portunda aktif.`));
